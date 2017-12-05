83be1420ffa6dd2793e497448340d4a84f66c124
Justin Chiu <jfc4050@gmail.com>
2017-12-05 14:04:31 -0800

    Initial Commit

---
+++ CS2C/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CS2C
+//
+//  Created by Justin Chiu on 12/5/17.
+//  Copyright © 2017 Justin Chiu. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
